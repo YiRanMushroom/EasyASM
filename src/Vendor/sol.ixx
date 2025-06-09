48bceb03121b79b9f572a5bd31755910e7573823
YiRanMushroom <yiranmushroom@gmail.com>
2025-06-09 08:45:20 -0700

    Add some information.

--- src/Vendor/sol.ixx
+++ src/Vendor/sol.ixx
@@ -1,2 +1,3 @@
 export module Vendor.sol;
+// This module provides the sol2 library for Lua bindings in C++.
 export import "sol2_build.h";
--- src/Vendor/sol2_build.h
+++ src/Vendor/sol2_build.h
@@ -1,3 +1,4 @@
 #pragma once
+// This does not helpful though, still does not have full support for exceptions
 #define SOL_EXCEPTIONS_SAFE_PROPAGATION 1
 #include <sol/sol.hpp>
